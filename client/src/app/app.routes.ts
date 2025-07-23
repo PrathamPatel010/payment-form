21dae22ffb4b7b4648709400289c0795033fb6d4
Pratham Patel <ppratham812@gmail.com>
2025-07-23 11:08:30 +0530

    bootstrapped the angular app

---
+++ client/src/app/app.routes.ts
@@ -0,0 +1,4 @@
+import { Routes } from '@angular/router';
+import { ContributionComponent } from './features/contribution/contribution.component';
+
+export const routes: Routes = [{ path: '', component: ContributionComponent }];
